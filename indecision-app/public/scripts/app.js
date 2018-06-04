ef9c9f177405d4659ce1c5d646149980995e5dd5
rahul shahare <rahulshahare@gmail.com>
2018-06-05 01:00:45 +0530

    React.createElement is been working

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,1 +1,14 @@
 console.log('App.js is running');
+
+//JSX = Javascript XML
+
+//var template = <p>This is JSX from app.js</p>;
+var template = React.createElement(
+    "p",
+    { id: "hello" },
+    "This is from app.js"
+  );
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template,appRoot);
