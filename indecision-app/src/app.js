7640438958e59d700db6a3c76e6e490febd16184
rahul shahare <rahulshahare@gmail.com>
2018-06-06 19:29:08 +0530

    Looks Beautful to eye.

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,7 +2,10 @@ console.log('App.js is running');
 
 //JSX = Javascript XML
 
-var template = <div><h1>This is changing.</h1><p>More info</p></div>;
+var template = <div>
+               <h1>This is changing.</h1>
+               <p>More info</p>
+               </div>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template,appRoot); 
