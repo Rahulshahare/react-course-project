35e5c879897283abc631a55d64554c185d9eeb90
rahul shahare <rahulshahare@gmail.com>
2018-06-06 15:09:16 +0530

    this is beautful thing. jsx is converted into javascript live. and showing live in browser

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,14 +1,14 @@
+'use strict';
+
 console.log('App.js is running');
 
 //JSX = Javascript XML
 
-//var template = <p>This is JSX from app.js</p>;
 var template = React.createElement(
-    "p",
-    { id: "hello" },
-    "This is from app.js"
-  );
-
+  'h1',
+  null,
+  'this is changing'
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template,appRoot);
+ReactDOM.render(template, appRoot);
---
+++ indecision-app/src/app.js
@@ -0,0 +1,8 @@
+console.log('App.js is running');
+
+//JSX = Javascript XML
+
+var template = <h1>this is changing</h1>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template,appRoot);
