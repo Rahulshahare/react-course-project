8be38e15ee8bf32287e27dc9bc6333a909530546
rahul shahare <rahulshahare@gmail.com>
2018-06-06 19:25:25 +0530

    FIRST ERROR: JSX element must be wrap in single enclosing tag. so h1 and p are enclose in div . and JSX goes on fire

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+console.log('App.js is running');
+
+//JSX = Javascript XML
+
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'This is changing.'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'More info'
+  )
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,7 +2,7 @@ console.log('App.js is running');
 
 //JSX = Javascript XML
 
-var template = <h1>This is changing.</h1><p>More info</p>;
+var template = <div><h1>This is changing.</h1><p>More info</p></div>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template,appRoot); 
