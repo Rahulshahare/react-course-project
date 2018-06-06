14076042b179ed793b23907c4470d544374ecf4c
rahul shahare <rahulshahare@gmail.com>
2018-06-06 19:20:08 +0530

    Dats an First Error of JSX. app.js from the source is empty bcz its an error

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,7 +2,7 @@ console.log('App.js is running');
 
 //JSX = Javascript XML
 
-var template = <h1>This is changing.</h1>;
+var template = <h1>This is changing.</h1><p>More info</p>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template,appRoot); 
