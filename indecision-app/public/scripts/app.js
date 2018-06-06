1b62dc4a8e17a6f0eadb849234656ebeeedfa662
rahul shahare <rahulshahare@gmail.com>
2018-06-06 19:32:36 +0530

    Nesting more elements ...

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -5,18 +5,42 @@ console.log('App.js is running');
 //JSX = Javascript XML
 
 var template = React.createElement(
-            'div',
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'This is changing.'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'More info'
+    ),
+    React.createElement(
+        'ul',
+        null,
+        React.createElement(
+            'li',
             null,
-            React.createElement(
-                        'h1',
-                        null,
-                        'This is changing.'
-            ),
-            React.createElement(
-                        'p',
-                        null,
-                        'More info'
-            )
+            'Hi this is one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Hi this is two'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Hi this is three'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Hi this is four'
+        )
+    )
 );
 var appRoot = document.getElementById('app');
 
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -6,6 +6,12 @@ var template = (
             <div>
             <h1>This is changing.</h1>
             <p>More info</p>
+            <ul>
+                <li>Hi this is one</li>
+                <li>Hi this is two</li>
+                <li>Hi this is three</li>
+                <li>Hi this is four</li>
+            </ul>
             </div>
 );
 var appRoot = document.getElementById('app');
