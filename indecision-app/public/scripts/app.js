97b3444e0faafc474e39a0d22992d6c80e1e45b5
rahul shahare <rahulshahare@gmail.com>
2018-06-06 15:34:34 +0530

    jsx is converting into javascript for usage

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,1 +1,14 @@
-"use strict";var template=React.createElement("h1",null,"This is changing..."),appRoot=document.getElementById("app");ReactDOM.render(template,appRoot);
+'use strict';
+
+console.log('App.js is running');
+
+//JSX = Javascript XML
+
+var template = React.createElement(
+  'h1',
+  null,
+  'This is changing'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,7 +2,7 @@ console.log('App.js is running');
 
 //JSX = Javascript XML
 
-var template = <h1>this is changing...</h1>;
+var template = <h1>This is changing</h1>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template,appRoot);
