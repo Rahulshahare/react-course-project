5733954e98fe7bd93d4480f3bdc130d0d9da7b58
rahul shahare <rahulshahare@gmail.com>
2018-06-06 15:22:50 +0530

    NODE MODULES can be download back from the package file

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log('App.js is running');
+
+//JSX = Javascript XML
+
+var template = React.createElement(
+  'h1',
+  null,
+  'this is changing'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
