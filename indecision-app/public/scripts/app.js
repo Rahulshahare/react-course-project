2990f750c311d0beaddd880bc2d7c1146e333681
rahul shahare <rahulshahare@gmail.com>
2018-06-10 00:52:05 +0530

    a function with argument

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,1 +1,12 @@
 "use strict";
+
+//argument object - no longer bound with arrow functions
+
+var add = function add(a, b) {
+    console.log(arguments);
+    return a + b;
+};
+
+console.log(add(55, 1));
+
+//this keyword - no longer bound
---
+++ indecision-app/src/playground/es6-arrow-function-2.js
@@ -0,0 +1,10 @@
+//argument object - no longer bound with arrow functions
+
+const add = function(a,b){
+    console.log(arguments);
+    return a + b;
+};
+
+console.log(add(55,1));
+
+//this keyword - no longer bound
