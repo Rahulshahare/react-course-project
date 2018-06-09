48de59171c814e976aab2e9e2d1ee4f8e4d5a3ba
rahul shahare <rahulshahare@gmail.com>
2018-06-09 15:53:57 +0530

    var let and const type

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var nameVar = 'Wizkumar';
+var nameVar = 'TGOD';
+console.log('namevar', nameVar);
+//var can be reassign and redeclare
+
+var nameLet = "its ssaturday";
+nameLet = "its Sunday";
+console.log('nameLet', nameLet);
+//let can be reassign but can not redeclare
+
+var nameConst = 'Taylor';
+console.log('nameConst', nameConst);
+//const can not reassign or redeclare
+
+//Block Scoping
--- indecision-app/src/playground/es6-let-const.js
+++ indecision-app/src/playground/es6-let-const.js
@@ -1,11 +1,15 @@
 var nameVar = 'Wizkumar';
 var nameVar = 'TGOD';
 console.log('namevar', nameVar);
+//var can be reassign and redeclare
 
 let nameLet = "its ssaturday";
 nameLet = "its Sunday";
 console.log('nameLet', nameLet);
+//let can be reassign but can not redeclare
 
 const nameConst = 'Taylor';
-const nameConst = 'update';
 console.log('nameConst', nameConst);
+//const can not reassign or redeclare
+
+//Block Scoping
