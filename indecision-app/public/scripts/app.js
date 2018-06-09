2c80f46081f759de4f08b468598a4b75996be0d3
rahul shahare <rahulshahare@gmail.com>
2018-06-10 00:57:54 +0530

    save

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,13 +1,23 @@
-"use strict";
+'use strict';
 
-var _arguments = arguments;
 //argument object - no longer bound with arrow functions
 
 var add = function add(a, b) {
-    console.log(_arguments);
+    //console.log(arguments);
     return a + b;
 };
 
 console.log(add(55, 1));
 
 //this keyword - no longer bound
+
+var user = {
+    name: 'rahul',
+    cities: ['pkd', 'mumbai', 'pune'],
+    printPlacesLived: function printPlacesLived() {
+        console.log(this.name);
+        console.log(this.cities);
+    }
+};
+
+user.printPlacesLived();
--- indecision-app/src/playground/es6-arrow-function-2.js
+++ indecision-app/src/playground/es6-arrow-function-2.js
@@ -1,10 +1,19 @@
 //argument object - no longer bound with arrow functions
 
 const add = (a,b) => {
-    console.log(arguments);
+    //console.log(arguments);
     return a + b;
 };
 
 console.log(add(55,1));
 
 //this keyword - no longer bound
+
+const user = {
+    name:'rahul',
+    cities:['pkd','mumbai','pune'],
+    printPlacesLived: function(){
+        console.log(this.name);
+        console.log(this.cities);
+    }
+};
