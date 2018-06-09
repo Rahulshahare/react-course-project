a606ba2cdc63a6cf6f25f63124b0e85dfa443eed
rahul shahare <rahulshahare@gmail.com>
2018-06-09 20:09:07 +0530

    This is es5 working function

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,65 +1,7 @@
-'use strict';
+"use strict";
 
-console.log('App.js is running');
-
-//var let const
-var app = {
-    title: 'Indicision App',
-    subtitle: 'Put your life in hands of computer',
-    options: ['one', 'two']
-};
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options.length > 0 ? "Here are your options" : "No Options"
-    )
-);
-
-var user = {
-    name: 'Rahul Shahare',
-    age: 25,
-    location: 'Pandharkawada, India'
+var square = function square(x) {
+    return x * x;
 };
 
-function getLocation(location) {
-    if (location) {
-        return React.createElement(
-            'p',
-            null,
-            'Location: ',
-            location
-        );
-    }
-}
-
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name ? user.name : 'Anonymous'
-    ),
-    user.age && user.age >= 18 && React.createElement(
-        'p',
-        null,
-        user.age
-    ),
-    getLocation(user.location)
-);
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
+console.log(square(9));
