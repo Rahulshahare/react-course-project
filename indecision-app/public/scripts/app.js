2ac399df357f654011160e9fbf4cdbab59c14b25
rahul shahare <rahulshahare@gmail.com>
2018-06-09 10:00:03 +0530

    here is the Error: TypeError: src/playground/es6-let-const.js: Duplicate declaration "nameLet". so we can not redefine the variabe.

--- indecision-app/public/scripts/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var nameVar = 'Wizkumar';
-var nameVar = 'TGOD';
-console.log('namevar', nameVar);
-
-var nameLet = "its ssaturday";
-nameLet = "its Sunday";
-console.log('nameLet', nameLet);
--- indecision-app/src/playground/es6-let-const.js
+++ indecision-app/src/playground/es6-let-const.js
@@ -3,5 +3,5 @@ var nameVar = 'TGOD';
 console.log('namevar', nameVar);
 
 let nameLet = "its ssaturday";
-nameLet = "its Sunday";
+let nameLet = "its Sunday";
 console.log('nameLet', nameLet);
