813d25f22c411023e40bf82232b47a19ef07d15b
rahul shahare <rahulshahare@gmail.com>
2018-06-11 20:51:47 +0530

    Challenge Complete

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -3,19 +3,25 @@
 //getDescription - Rahul shahare is 26 year(s) old.
 
 class Person {
-    constructor(name  = 'Anynomous'){
+    constructor(name  = 'Anynomous', age = 0){
         this.name = name;
+        this.age = age;
     }
 
     getGreeting(){
         //return 'Hello i am ' + this.name + '!';
         return`Hi! i am ${this.name}`;
     }
+    getDescription(){
+        return`${this.name} is ${this.age} year(s) old.`;
+    }
 }
 
-const me = new Person('Rahul Shahare');
+const me = new Person('Rahul Shahare',26);
 console.log(me.getGreeting());
+console.log(me.getDescription());
 
 const other = new Person();
 console.log(other.getGreeting());
+console.log(other.getDescription());
 
