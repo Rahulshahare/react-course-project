4d4b3047b7017e0a1fc850007df910c38a113291
rahul shahare <rahulshahare@gmail.com>
2018-06-11 20:44:35 +0530

    a function within a class can be access like this

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,19 +1,32 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 //
 
-var Person = function Person() {
-    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anynomous';
+var Person = function () {
+    function Person() {
+        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anynomous';
 
-    _classCallCheck(this, Person);
+        _classCallCheck(this, Person);
 
-    this.name = name;
-};
+        this.name = name;
+    }
+
+    _createClass(Person, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            return 'Hello';
+        }
+    }]);
+
+    return Person;
+}();
 
 var me = new Person('Rahul Shahare');
-console.log(me);
+console.log(me.getGreeting());
 
 var other = new Person();
-console.log(other);
+console.log(other.getGreeting());
--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -4,11 +4,15 @@ class Person {
     constructor(name  = 'Anynomous'){
         this.name = name;
     }
+
+    getGreeting(){
+        return 'Hello';
+    }
 }
 
 const me = new Person('Rahul Shahare');
-console.log(me);
+console.log(me.getGreeting());
 
 const other = new Person();
-console.log(other);
+console.log(other.getGreeting());
 
