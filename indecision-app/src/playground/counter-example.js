e61643b101ad4d28e111dbef52843c347285ec6b
rahul shahare <rahulshahare@gmail.com>
2018-06-15 00:37:16 +0530

    dats all

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -51,38 +51,4 @@ Counter.defaultProps ={
     count:0
 };
 
-ReactDOM.render(<Counter count={102}/>, document.getElementById('app'));
-/*let count = 0;
-const addOne = ()=>{
-    console.log("addOne");
-    count++;
-    renderCounterApp();
-};
-const minusOne = ()=>{
-    console.log("minusOne");
-    count--;
-    renderCounterApp();
-};
-const reset = ()=>{
-    console.log("Reset");
-    count = 0;
-    renderCounterApp();
-};
-
-const appRoot = document.getElementById('app');
-
-
-const renderCounterApp = () =>{
-    const templateTwo = (
-        <div>
-            <h1>Count:{count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={reset}>Reset</button>
-            <button onClick={minusOne}>-1</button>
-        </div>
-    ); 
-    ReactDOM.render(templateTwo,appRoot); 
-
-};
-
-renderCounterApp();*/
+ReactDOM.render(<Counter/>, document.getElementById('app'));
