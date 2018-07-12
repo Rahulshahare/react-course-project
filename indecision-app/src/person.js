675b24a16a33b5161336a8e8d94629452255a4d0
rahul shahare <rahulshahare@gmail.com>
2018-07-12 21:48:38 +0530

    SHortHand

--- indecision-app/src/person.js
+++ indecision-app/src/person.js
@@ -1,15 +1,3 @@
-export const isAdult = ( age ) =>{
-    if(age >= 18){
-        return true;
-    }else{
-        return false;
-    }
-};
+export const isAdult = ( age ) => age >= 18;
 
-export const canDrink = ( age ) =>{
-    if(age >= 21){
-        return true;
-    }else{
-        return false;
-    }
-};  
+export const canDrink = ( age ) => age>= 21;  
