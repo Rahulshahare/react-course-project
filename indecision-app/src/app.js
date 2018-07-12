82cc049b5c29ec8967a97d086356a6adcbc4773c
rahul shahare <rahulshahare@gmail.com>
2018-07-12 06:19:30 +0530

    just tweek the component data to check its working or not

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -64,7 +64,7 @@ class IndecisionApp extends React.Component{
         const subtitle = 'Put Your life in the hands of Computer';
         return(
             <div>
-                <Header title={title}  subtitle={subtitle}/>
+                <Header   subtitle={subtitle}/>
                 <Action hasOptions={this.state.options.length > 0}
                         handlePick = {this.handlePick}/>
                 <Options 
