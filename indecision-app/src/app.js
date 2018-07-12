ba5f66e22b46854bc15d7ee26061c9f32d293737
rahul shahare <rahulshahare@gmail.com>
2018-07-12 21:04:59 +0530

    game goes on

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,5 +1,6 @@
 //import './utils.js';
-import { square } from './utils.js';
+import { square, add } from './utils.js';
 
 console.log('app.js is Running');
 console.log(square(4));
+console.log(add(100, 23));
--- indecision-app/src/utils.js
+++ indecision-app/src/utils.js
@@ -2,8 +2,8 @@ console.log('utils.js is Running');
 
 const square = (x) => x * x;
 
+const add = (a,b) => a + b;
 
-
-export { square };
+export { square, add };
 
 // exports - default export - named export
