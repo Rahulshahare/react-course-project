e5a62b62b8c1363656b0ca85d32d9160b7efa1d0
rahul shahare <rahulshahare@gmail.com>
2018-07-12 21:08:56 +0530

    challenge time

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,10 @@
-//import './utils.js';
-import { square, add } from './utils.js';
+// //import './utils.js';
+// import { square, add } from './utils.js';
 
-console.log('app.js is Running');
-console.log(square(4));
-console.log(add(100, 23));
+// console.log('app.js is Running');
+// console.log(square(4));
+// console.log(add(100, 23));
+
+//persona.js
+//isAdult(18) - true or false
+// canDrink(18) true if 21
