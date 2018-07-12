f265870f67d295fa7d480c68fcd975c9d651ef98
rahul shahare <rahulshahare@gmail.com>
2018-07-12 15:25:34 +0530

    this is showing a absolute path for output folder

--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -1,5 +1,10 @@
 // Entry -> output
-
+const path =require('path');
+console.log(path.join(__dirname, 'public')); 
 module.exports = {
-    entry: './src/app.js'
+    entry: './src/app.js',
+    output : {
+        path : '',
+        filename : 'bundle.js'
+    }
 };
