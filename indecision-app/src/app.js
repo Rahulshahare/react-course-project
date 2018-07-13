8333316f861533ba0d1db9a1e1db1ab46bd4c74e
rahul shahare <rahulshahare@gmail.com>
2018-07-13 19:18:01 +0530

    New challenge

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,12 +1,16 @@
 // //import './utils.js';
-import subtract, { square, add } from './utils.js';
+// import subtract, { square, add } from './utils.js';
 
-console.log('app.js is Running');
-console.log(square(4));
- console.log(subtract(100, 23));
+// console.log('app.js is Running');
+// console.log(square(4));
+//  console.log(subtract(100, 23));
 
 
-// import {isAdult , canDrink } from './person.js';
+import {isAdult , canDrink } from './person.js';
 
-// console.log( isAdult(15));
-// console.log( canDrink(25));
+console.log( isAdult(15));
+console.log( canDrink(25));
+console.log(isSenior(65));
+
+//setting default export and function
+// grab teh default and call the function
