295ac20252dbf59aa1c3754ce1080d18371dbce2
rahul shahare <rahulshahare@gmail.com>
2018-07-13 20:18:22 +0530

    importing NPM modeles

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,16 +1,1 @@
-// //import './utils.js';
-// import subtract, { square, add } from './utils.js';
-
-// console.log('app.js is Running');
-// console.log(square(4));
-//  console.log(subtract(100, 23));
-
-
-import isSenior, {isAdult , canDrink } from './person.js';
-
-console.log( isAdult(15));
-console.log( canDrink(25));
-console.log( isSenior(63));
-
-//setting default export and function
-// grab teh default and call the function
+// install ->import -> use
